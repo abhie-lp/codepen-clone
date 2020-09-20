0fd898f58689f66cb96702775bd099a899f554df
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-21 00:50:34 +0530

    Install codemirror and Editor component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+
+// Components
+import Editor from "./Editor";
+
 function App() {
-  return "Hello world!!";
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
 }
 
 export default App;
