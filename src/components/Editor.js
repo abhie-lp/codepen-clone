341a71ed89d9b510fa350e02c2c048565c501ef6
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-22 00:46:20 +0530

    Show basic editor for html, css, js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
-const Editor = () => {
+// packages
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
+
+import {Controlled as ControlledEditor} from "react-codemirror2";
+
+const Editor = (props) => {
+  const {
+    language,
+    displayName,
+    value,
+    onChange
+  } = props
+  
+  const handleChange = (editor, data, value) => {
+    onChange(value)
+  }
+  
   return (
-    <div>
-      Editor
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true
+        }}
+      />
     </div>
   );
 };
